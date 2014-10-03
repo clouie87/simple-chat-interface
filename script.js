fad6c140b8c193f5b1804b1d6c37f2f76eba62ec
clouie87 <carol.louie@gmail.com>
2014-10-03 08:59:37 -0300

    giant social media logos :(

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 
 
+/*
+function myFacebook(){
+	href="https://www.facebook.com/carol.louie"
+};
+document.write() */
+
+/*<script> 
+function chatbox(){
+	var cb= document.getElementById("type here");
+
+}*/
+
+//to make the "Join Us" button work 
+
+function join(){
+	var join = prompt ("What is your First Name?");
+}
+
+function chatmessage(){
+
+
+}
