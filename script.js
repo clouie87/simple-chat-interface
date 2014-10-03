d986184dccd7adbda97eafa7e30f1b4e692750a2
clouie87 <carol.louie@gmail.com>
2014-10-03 11:45:43 -0300

    This is creating a break element in my chat box

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ function chatbox(){
 /*function(){
 
 }*/
+/*
+function moveAbout(){
+	document.scroll = "scroolBottom()"
+} I havent figured out what the fuck i am doing here but i will! this will be sorted :)! 
+*/
 
 function join(){
 	var join = prompt ("What is your First Name?");
@@ -21,8 +26,11 @@ function join(){
 console.log("scriptloader")
 function mySubmit(){
 	var message = document.getElementById("usermsg").value;
+		//
 	console.log("message is "+message);
 	document.getElementById("chatbox").appendChild(document.createTextNode(message));
-	document.getElementById("usermsg").value="";
+		//this goes to my 'Chatbox' on my html and will add (append) what is written in my messages to my chatbox
+	document.getElementById("chatbox").appendChild(document.createElement("br"));
+	document.getElementById("usermsg").value=""; //this means that the message gets cleared after it is sent
 	return false;// this lets me submit with enter and not have to use to submit button (must create html onsubmit=mySubmit())
 }
