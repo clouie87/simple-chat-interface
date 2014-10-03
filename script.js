3276d22c8dd86fe0bf08daa7ff42ac094b79fe33
clouie87 <carol.louie@gmail.com>
2014-10-03 10:49:56 -0300

    submit by enter not clicking submit button

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ function chatbox(){
 	var cb= document.getElementById("type here");
 
 }*/
-//buttom
+/*function(){
+
+}*/
+
 function join(){
 	var join = prompt ("What is your First Name?");
 }
-
 console.log("scriptloader")
 function mySubmit(){
 	var message = document.getElementById("usermsg").value;
 	console.log("message is "+message);
 	document.getElementById("chatbox").appendChild(document.createTextNode(message));
+	document.getElementById("usermsg").value="";
+	return false;// this lets me submit with enter and not have to use to submit button (must create html onsubmit=mySubmit())
 }
